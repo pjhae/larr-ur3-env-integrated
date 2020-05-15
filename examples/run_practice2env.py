e29975b49bef9525e6ba8265301f7b6e755c2495
Jigang Kim <jgkim2020@snu.ac.kr>
2020-05-15 22:21:13 +0900

    Double pendulum test

---
+++ examples/run_practice2env.py
@@ -0,0 +1,22 @@
+import numpy as np
+import time
+
+import gym_custom
+
+def run_dynamics():
+    env = gym_custom.make('Practice2-motor-v0')
+    obs = env.reset()
+    dt = env.env.dt
+
+    for _ in range(int(60/dt)):
+        action = env.action_space.sample()
+        action = np.zeros_like(action)
+        obs, _, _, _ = env.step(action)
+        qpos, qvel = obs[:env.env.model.nq], obs[-env.env.model.nv:]
+
+        env.render()
+        print('qpos: %s (degrees), qvel: %s (dps), qfrc_bias: %s'%(qpos, qvel, env.env.sim.data.qfrc_bias))
+        time.sleep(dt)
+
+if __name__ == '__main__':
+    run_dynamics()
