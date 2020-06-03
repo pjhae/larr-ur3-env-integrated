c0961451c725ccefa1b19e66553024b255a2d1ce
Jigang Kim <jgkim2020@snu.ac.kr>
2020-06-03 22:10:01 +0900

    Kinematics for Dual UR3 (in progress)

--- examples/test_dualur3env.py
+++ examples/test_dualur3env.py
@@ -9,27 +9,59 @@ def run_dual_ur3():
     obs = env.reset()
     dt = env.dt
 
-    for _ in range(int(60/dt)):
+    for t in range(int(60/dt)):
         action = env.action_space.sample()
         # action = np.zeros_like(action)
         obs, _, _, _ = env.step(action)
         env.render()
+        print('time: %.2f'%(t*dt))
+        print('  joint_pos: %s (rad)'%(env._get_ur3_qpos()*180/np.pi))
+        print('  joint_vel: %s (rad/s)'%(env._get_ur3_qvel()*180/np.pi))
+        print('  joint_bias: %s (Nm)'%(env._get_ur3_bias()*180/np.pi))
+        print('  gripper_pos: %s (m)'%(env._get_gripper_qpos()[[2,7,12,17]]))
+        print('  gripper_vel: %s (m/s)'%(env._get_gripper_qvel()[[2,7,12,17]]))
+        print('  gripper_bias: %s (N)'%(env._get_gripper_bias()[[2,7,12,17]]))
         time.sleep(dt)
+
+def test_fkine_ikine():
+    env = gym_custom.make('dual-ur3-larr-v0')
+    obs = env.reset()
+    dt = env.dt
+
+    # test forward kinematics
+    q = env._get_ur3_qpos()[:env.ur3_nqpos] # right
+    Rs, ps, Ts = env.forward_kinematics_DH(q, arm='right')
+    R_base, p_base, T_base = env.forward_kinematics(body_name='right_arm_rotz')
+    # R_hand, p_hand, T_hand = env.forward_kinematics(body_name='right_gripper:hand')
+    R_hand, p_hand, T_hand = env.forward_kinematics(body_name='right_ee_link')
+    print('base:')
+    print('  pos: (DH) %s vs. (MjData) %s'%(ps[0,:], p_base))
+    print('  rotMat: (DH) %s vs. (MjData) %s'%(Rs[0,:,:], R_base))
+    print('hand:')
+    print('  pos: (DH) %s vs. (MjData) %s'%(ps[-1,:], p_hand))
+    print('  rotMat: (DH) %s vs. (MjData) %s'%(Rs[-1,:,:], R_hand))
 
 def servoj_and_forceg():
     env = gym_custom.make('dual-ur3-larr-v0')
     obs = env.reset()
     dt = env.dt
 
+    for _ in range():
+        pass
+
 def speedj_and_forceg():
     env = gym_custom.make('dual-ur3-larr-v0')
     obs = env.reset()
     dt = env.dt
+
+    for _ in range():
+        pass
 
 def pick_and_place():
     pass
 
 if __name__ == '__main__':
     # run_dual_ur3()
+    test_fkine_ikine()
     # servoj_and_forceg()
-    speedj_and_forceg()
+    # speedj_and_forceg()
--- gym_custom/envs/custom/dual_ur3_env.py
+++ gym_custom/envs/custom/dual_ur3_env.py
@@ -25,8 +25,26 @@ class DualUR3Env(MujocoEnv, utils.EzPickle):
             np.array([-90.0, -90.0, -90.0, -45.0, 225.0, 0.0])*np.pi/180.0 # right arm
         self.init_qpos[self.ur3_nqpos+self.gripper_nqpos:2*self.ur3_nqpos+self.gripper_nqpos] = \
             np.array([90.0, -90.0, 90.0, 225.0, 135.0, 0.0])*np.pi/180.0 # left arm
-        # Initial postion for gripper
-        self.init_qpos[8], self.init_qpos[13] = 0.04, 0.04
+        
+        # Settings for forward/inverse kinematics
+        # https://www.universal-robots.com/articles/ur-articles/parameters-for-calculations-of-kinematics-and-dynamics/
+        self.kinematics_params = {}
+        self.kinematics_params['d'] = np.array([0.1519, 0, 0, 0.11235, 0.08535, 0.0819+0.12*0]) # in m
+        self.kinematics_params['a'] = np.array([0, -0.24365, -0.21325, 0, 0, 0]) # in m
+        self.kinematics_params['alpha'] =np.array([np.pi/2, 0, 0, np.pi/2, -np.pi/2, 0]) # in rad
+        self.kinematics_params['offset'] = np.array([0, 0, 0, 0, 0, 0])
+        self.kinematics_params['ub'] = np.array([2*np.pi for _ in range(6)])
+        self.kinematics_params['lb'] = np.array([-2*np.pi for _ in range(6)])
+        
+        self.kinematics_params['T_wb_right'] = np.eye(4)
+        right_base_idx = self.model.body_names.index('right_arm_rotz')
+        self.kinematics_params['T_wb_right'][0:3,0:3] = self.sim.data.body_xmat[right_base_idx].reshape([3,3])
+        self.kinematics_params['T_wb_right'][0:3,3] = self.sim.data.body_xpos[right_base_idx]
+        
+        self.kinematics_params['T_wb_left'] = np.eye(4)
+        left_base_idx = self.model.body_names.index('left_arm_rotz')
+        self.kinematics_params['T_wb_left'][0:3,0:3] = self.sim.data.body_xmat[left_base_idx].reshape([3,3])
+        self.kinematics_params['T_wb_left'][0:3,3] = self.sim.data.body_xpos[left_base_idx]
 
     def step(self, a):
         reward = 1.0
@@ -41,32 +59,169 @@ class DualUR3Env(MujocoEnv, utils.EzPickle):
         self.set_state(qpos, qvel)
         return self._get_obs()
 
+    def forward_kinematics_DH(self, q, arm):
+        assert len(q) == self.ur3_nqpos
+
+        if arm == 'right':
+            T_0_i = self.kinematics_params['T_wb_right']
+        elif arm == 'left':
+            T_0_i = self.kinematics_params['T_wb_left']
+        else:
+            raise ValueError('Invalid arm type!')
+        T = np.zeros([self.ur3_nqpos+1, 4, 4])
+        R = np.zeros([self.ur3_nqpos+1, 3, 3])
+        p = np.zeros([self.ur3_nqpos+1, 3])
+        # Base frame
+        T[0,:,:] = T_0_i
+        R[0,:,:] = T_0_i[0:3,0:3]
+        p[0,:] = T_0_i[0:3,3]
+
+        for i in range(self.ur3_nqpos):
+            ct = np.cos(q[i] + self.kinematics_params['offset'][i])
+            st = np.sin(q[i] + self.kinematics_params['offset'][i])
+            ca = np.cos(self.kinematics_params['alpha'][i])
+            sa = np.sin(self.kinematics_params['alpha'][i])
+
+            T_i_iplus1 = np.array([[ct, -st*ca, st*sa, self.kinematics_params['a'][i]*ct],
+                                   [st, ct*ca, -ct*sa, self.kinematics_params['a'][i]*st],
+                                   [0, sa, ca, self.kinematics_params['d'][i]],
+                                   [0, 0, 0, 1]])
+            T_0_i = np.matmul(T_0_i, T_i_iplus1)
+            # cf. base frame at i=0
+            T[i+1, :, :] = T_0_i
+            R[i+1, :, :] = T_0_i[0:3,0:3]
+            p[i+1, :] = T_0_i[0:3,3]
+
+        return R, p, T
+
+    def _jacobian_DH(self, q, arm):
+        assert len(q) == self.ur3_nqpos
+        epsilon = 1e-6
+        epsilon_inv = 1/epsilon
+        _, _, ps = self.forward_kinematics_DH(q, arm)
+        p = ps[-1,:] # unperturbed position
+
+        jac = np.zeros([3, self.ur3_nqpos])
+        for i in range(self.ur3_nqpos):
+            q_ = q.copy()
+            q_[i] = q_[i] + epsilon
+            _, _, ps_ = self.forward_kinematics_DH(q_, arm)
+            p_ = ps_[-1,:] # perturbed position
+            jac[:, i] = (p_ - p)*epsilon_inv
+
+        return jac
+
+    def forward_kinematics(self, body_name):
+        # assert len(q) == self.ur3_nqpos
+        body_idx = self.model.body_names.index(body_name)
+        R = self.sim.data.body_xmat[body_idx].reshape([3,3])
+        p = self.sim.data.body_xpos[body_idx]
+        T = np.eye(4)
+        T[0:3,0:3] = R
+        T[0:3,3] = p
+
+        return R, p, T
+
+    # def _jacobian(self, q, body_name):
+    #     assert len(q) == self.ur3_nqpos
+    #     epsilon = 1e-6
+    #     epsilon_inv = 1/epsilon
+    #     _, _, p = self.forward_kinematics(q, body_name) # unperturbed position
+
+    #     jac = np.zeros([3, self.ur3_nqpos])
+    #     for i in range(self.ur3_nqpos):
+    #         q_ = q.copy()
+    #         q_[i] = q_[i] + epsilon
+    #         _, _, p_ = self.forward_kinematics(q_, body_name) # perturbed position
+    #         jac[:, i] = (p_ - p)*epsilon_inv
+
+    #     return jac
+
+    def inverse_kinematics_ee(self, ee_pos, null_obj_func, arm,
+            q_init='current', threshold=0.01, threshold_null=0.001, max_iter=100, epsilon=1e-6
+        ):
+        '''
+        inverse kinematics with forward_kinematics() and _jacobian()
+        '''
+        # Set initial guess
+        if arm == 'right':
+            if q_init == 'current': q = self._get_ur3_qpos()[:self.ur3_nqpos]
+            elif q_init == 'zero': q = np.zeros([self.ur3_nqpos])
+            elif type(q_init).__name__ == 'ndarray': q = q_init
+            else: raise ValueError("q_init must be one of the following: ['current', 'zero', numpy.ndarray]")
+        elif arm == 'left':
+            if q_init == 'current': q = self._get_ur3_qpos()[self.ur3_nqpos:]
+            elif q_init == 'zero': q = np.zeros([self.ur3_nqpos])
+            elif type(q_init).__name__ == 'ndarray': q = q_init
+            else: raise ValueError("q_init must be one of the following: ['current', 'zero', numpy.ndarray]")
+        else:
+            raise ValueError('Invalid arm type!')
+
+        arm_to_body_name = {'right': 'right_gripper:hand', 'left': 'left_gripper:hand'}
+        
+        R, x, _ = self.forward_kinematics(q, arm_to_body_name[arm])
+        jac = self._jacobian(q, arm_to_body_name[arm])
+        delta_x = ee_pos - x
+        err = np.linalg.norm(delta_x)
+        null_obj_val = null_obj_func(R)
+        iter_taken = 0
+
+        while True:
+            if (err < threshold and null_obj_val < threshold_null) or iter_taken >= max_iter: break
+            else: iter_taken += 1
+
+            # pseudo-inverse + null-space approach
+            jac_dagger = np.linalg.pinv(jac)
+            jac_null = np.eye(self.ur3_nqpos) - np.matmul(jac_dagger, jac) # null space of Jacobian
+            phi = np.zeros(self.ur3_nqpos) # find phi (null objective derivative)
+            for i in range(self.ur3_nqpos):
+                q_perturb = q.copy()
+                q_perturb[i] += epsilon
+                null_obj_val_perturb = null_obj_func.evaluate(q_perturb)
+                phi[i] = (null_obj_val_perturb - null_obj_val)/epsilon
+            # update
+            delta_x = ee_pos - x
+            delta_q = np.matmul(jac_dagger, delta_x) - np.matmul(jac_null, phi)
+            q += delta_q
+            q = np.minimum(self.kinematic_params['ub'], np.maximum(q, self.kinematic_params['lb'])) # clip within theta bounds
+            R, x, _ = self.forward_kinematics(q, arm_to_body_name[arm])
+            jac = self._jacobian(q, arm_to_body_name[arm])
+            null_obj_val = null_obj_func(R)
+            # evaluate
+            err = np.linalg.norm(delta_x)
+        
+        if iter_taken == max_iter:
+            warnings.warn('Max iteration limit reached! err: %f (threshold: %f), null_obj_err: %f (threshold: %f)'%(err, threshold, null_obj_val, threshold_null),
+                RuntimeWarning)
+        
+        return q, iter_taken, err, null_obj_val
+
     def _get_obs(self):
         return np.concatenate([self.sim.data.qpos, self.sim.data.qvel]).ravel()
 
     def _get_ur3_qpos(self):
         return np.concatenate([self.sim.data.qpos[0:self.ur3_nqpos], 
-            self.sim.data.qpos[self.ur3_nqpos+gripper_nqpos:2*self.ur3_nqpos+gripper_nqpos]]).ravel()
+            self.sim.data.qpos[self.ur3_nqpos+self.gripper_nqpos:2*self.ur3_nqpos+self.gripper_nqpos]]).ravel()
 
     def _get_gripper_qpos(self):
         return np.concatenate([self.sim.data.qpos[self.ur3_nqpos:self.ur3_nqpos+self.gripper_nqpos], 
-            self.sim.data.qpos[2*self.ur3_nqpos+gripper_nqpos:2*self.ur3_nqpos+2*gripper_nqpos]]).ravel()
+            self.sim.data.qpos[2*self.ur3_nqpos+self.gripper_nqpos:2*self.ur3_nqpos+2*self.gripper_nqpos]]).ravel()
 
     def _get_ur3_qvel(self):
         return np.concatenate([self.sim.data.qvel[0:self.ur3_nqvel], 
-            self.sim.data.qvel[self.ur3_nqvel+gripper_nqvel:2*self.ur3_nqvel+gripper_nqvel]]).ravel()
+            self.sim.data.qvel[self.ur3_nqvel+self.gripper_nqvel:2*self.ur3_nqvel+self.gripper_nqvel]]).ravel()
 
     def _get_gripper_qvel(self):
         return np.concatenate([self.sim.data.qvel[self.ur3_nqvel:self.ur3_nqvel+self.gripper_nqvel], 
-            self.sim.data.qvel[2*self.ur3_nqvel+gripper_nqvel:2*self.ur3_nqvel+2*gripper_nqvel]]).ravel()
+            self.sim.data.qvel[2*self.ur3_nqvel+self.gripper_nqvel:2*self.ur3_nqvel+2*self.gripper_nqvel]]).ravel()
 
     def _get_ur3_bias(self):
         return np.concatenate([self.sim.data.qfrc_bias[0:self.ur3_nqvel], 
-            self.sim.data.qfrc_bias[self.ur3_nqvel+gripper_nqvel:2*self.ur3_nqvel+gripper_nqvel]]).ravel()
+            self.sim.data.qfrc_bias[self.ur3_nqvel+self.gripper_nqvel:2*self.ur3_nqvel+self.gripper_nqvel]]).ravel()
 
     def _get_gripper_bias(self):
-        return np.concatenate([self.sim.data.qfrc_bias[0:self.ur3_nqvel], 
-            self.sim.data.qfrc_bias[self.ur3_nqvel+gripper_nqvel:2*self.ur3_nqvel+gripper_nqvel]]).ravel()
+        return np.concatenate([self.sim.data.qfrc_bias[self.ur3_nqvel:self.ur3_nqvel+self.gripper_nqvel], 
+            self.sim.data.qfrc_bias[2*self.ur3_nqvel+self.gripper_nqvel:2*self.ur3_nqvel+2*self.gripper_nqvel]]).ravel()
 
     def viewer_setup(self):
         v = self.viewer
@@ -173,15 +328,15 @@ class URScriptWrapper(ActionWrapper):
     def _positiong(self, q):
         assert q.shape[0] == self.ngripperdof
         bias = self.env._get_ur3_bias() # Internal forces
-        # TODO:
-        raise NotImplementedError
+        err = np.array([q[0], q[0], q[1], q[1]]) - self.env._get_gripper_qpos()
+        action = scale_factor_gripper*err + bias # P control
         return action
     
     def _velocityg(self, qd):
         assert q.shape[0] == self.ngripperdof
         bias = self.env._get_ur3_bias() # Internal forces
-        # TODO:
-        raise NotImplementedError
+        err = np.array([qd[0], qd[0], qd[1], qd[1]]) - self.env._get_gripper_qvel()
+        action = scale_factor_gripper*err + bias # P control
         return action
 
     def _forceg(self, qf):
