b3249834e025012b0b6789a6a488228f25159731
Jigang Kim <jgkim2020@snu.ac.kr>
2020-07-17 21:33:00 +0900

    Initial implementation

--- gym_custom/envs/real/ur/interface.py
+++ gym_custom/envs/real/ur/interface.py
@@ -1,8 +1,96 @@
+import numpy as np
+
 import gym_custom.envs.real.ur.drivers.URBasic
 
-# TODO
+
+COMMAND_LIMITS = {
+    'movej': [np.array([-2*np.pi, -2*np.pi, -np.pi, -2*np.pi, -2*np.pi, -np.inf]),
+        np.array([2*np.pi, 2*np.pi, np.pi, 2*np.pi, 2*np.pi, np.inf])], # [rad]
+    'speedj': [np.array([-np.pi, -np.pi, -np.pi, -2*np.pi, -2*np.pi, -2*np.pi]),
+        np.array([np.pi, np.pi, np.pi, 2*np.pi, 2*np.pi, 2*np.pi])], # [rad/s]
+}
+
 class URScriptInterface(object):
     
-    def __init__(self):
+    def __init__(self, host_ip):
+        self.model = URBasic.robotModel.RobotModel()
+        self.comm = URBasic.urScriptExt.UrScriptExt(host=host_ip, robotModel=self.model)
 
+    def close(self):
+        self.comm.close()
+
+    ## UR Controller
+    def reset_controller(self):
+        self.comm.reset_error()
+
+    ## UR3 manipulator
+    def movej(self, *args, **kwargs):
+        '''
+        Move to position (linear in joint-space)
+        blocking command, not suitable for online control
+        '''
+        self.comm.movej(*args, **kwargs)
+
+    def movel(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def movep(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def movec(self, *args, **kwargs):
+        raise NotImplementedError()
+    
+    def servoc(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def servoj(self, *args, **kwargs):
+        '''
+        Servo to position (linear in joint-space)
+        non-blocking command, suitable for online control
+        '''
+        self.comm.servoj(*args, **kwargs)
+
+    def speedj(self, *args, **kwargs):
+        '''
+        non-blocking command, suitable for online control
+        '''
+        self.comm.speedj(*args, **kwargs)
+
+    def speedl(self, *args, **kwargs):
+        raise NotImplementedError()
+    
+    def stopj(self, *args, **kwargs):
+        '''
+        ?
+        '''
+        self.comm.stopj(*args, **kwargs)
+
+    def stopl(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def get_joint_positions(self):
+        return np.array(self.comm.get_actual_joint_positions())
+
+    def get_joint_speeds(self):
+        return np.array(self.comm.get_actual_joint_speeds())
+
+    ## 2F-85 gripper
+    def open_gripper(self, *args, **kwargs):
+        # TODO: dscho
+        raise NotImplementedError()
+
+    def close_gripper(self, *args, **kwargs):
+        # TODO: dscho
+        raise NotImplementedError()
+
+    def move_gripper(self, *args, **kwargs):
+        # TODO: dscho
+        raise NotImplementedError()
+
+    def get_gripper_position(self):
+        # TODO: dscho
+        raise NotImplementedError()
+
+    def get_gripper_speed(self):
+        # TODO: dscho
         raise NotImplementedError()
--- gym_custom/envs/real/ur3.py
+++ gym_custom/envs/real/ur3.py
@@ -1,8 +1,117 @@
-from gym_custom.envs.real.ur.interface import URScriptInterface
+import gym_custom
+from gym_custom.envs.real.ur.interface import URScriptInterface, COMMAND_LIMITS
 
-# TODO
-class UR3ScriptEnv(URScriptInterface):
+
+def convert_action_to_space(action_limits=COMMAND_LIMITS):
+    if isinstance(action_limits, dict):
+        space = spaces.Dict(OrderedDict([
+            (key, convert_observation_to_space(value))
+            for key, value in COMMAND_LIMITS.items()
+        ]))
+    elif isinstance(action_limits, list):
+        low = action_limits[0]
+        high = action_limits[1]
+        space = gym_custom.spaces.Box(low, high, dtype=action_limits.dtype)
+    else:
+        raise NotImplementedError(type(action_limits), action_limits)
+
+    return space
+
+def convert_observation_to_space(observation):
+    if isinstance(observation, dict):
+        space = spaces.Dict(OrderedDict([
+            (key, convert_observation_to_space(value))
+            for key, value in observation.items()
+        ]))
+    elif isinstance(observation, np.ndarray):
+        low = np.full(observation.shape, -float('inf'), dtype=np.float32)
+        high = np.full(observation.shape, float('inf'), dtype=np.float32)
+        space = spaces.Box(low, high, dtype=observation.dtype)
+    else:
+        raise NotImplementedError(type(observation), observation)
+
+    return space
+
+class UR3RealEnv(gym_custom.Env):
     
-    def __init__(self):
-        raise NotImplementedError()
-        super(UR3ScriptEnv, self).__init__()
+    def __init__(self, host_ip, rate):
+        self.host_ip = host_ip
+        self.interface = URScriptInterface(host_ip)
+
+        # UR3 (6DOF), 2F-85 gripper (1DOF)
+        self._init_qpos = np.zeros([6])
+        self._init_qvel = np.zeros([6])
+        self._init_gripperpos = np.zeros([1])
+        self._init_grippervel = np.zeros([1])
+
+        self._set_action_space()
+        obs = self._get_obs()
+        self._set_observation_space(obs)
+
+        self._episode_step = None
+
+    def set_initial_joint_pos(self, q=None):
+        if q is None: pass
+        elif q == 'current': self._init_qpos = self.interface.get_joint_positions()
+        else: self._init_qpos = q
+        print('Initial joint position is set to %s'%(q))
+    
+    def set_initial_joint_vel(self, qd=None):
+        if qd is None: pass
+        elif qd == 'current': self._init_qvel = self.interface.get_joint_speeds()
+        else: self._init_qvel = qd
+        print('Initial joint velocity is set to %s'%(qd))
+
+    def set_initial_gripper_pos(self, g=None):
+        if g is None: pass
+        elif g == 'current': self._init_gripperpos = self.interface.get_gripper_position()
+        else: self._init_gripperpos = g
+        print('Initial gripper position is set to %s'%(g))
+
+    def set_initial_gripper_vel(self, gd=None):
+        if gd is None: pass
+        elif gd == 'current': self._init_grippervel = self.interface.get_gripper_position()
+        else: self._init_grippervel = gd
+        print('Initial gripper velocity is set to %s'%(gd))
+
+    def step(self, action):
+        assert self._episode_step is not None, 'Must reset before step!'
+        for command in action:
+            getattr(self.interface, command['type'])(command['data'])
+
+    def reset(self):
+        self.interface.reset_controller()
+        ob = self.reset_model()
+
+    def render(self, mode='human'):
+        warnings.warn('Real environment. "Render" with your own two eyes!')
+
+    def close(self):
+        self.interface.close()
+
+    def reset_model(self):
+        self.interface.movej(q=self._init_qpos)
+        self.interface.move_gripper(q=self._init_gripperpos)
+        self._episode_step = 0
+
+    def _get_obs(self):
+        return {
+            'qpos': self.interface.get_joint_positions(),
+            'qvel': self.interface.get_joint_speeds(),
+            'gripperpos': self.interface.get_gripper_position(),
+            'grippervel': self.interface.get_gripper_speed()
+        }
+
+    def _set_action_space(self):
+        self.action_space = convert_action_to_space()
+        return self.action_space
+
+    def _set_observation_space(self, observation):
+        self.observation_space = convert_observation_to_space(observation)
+        return self.observation_space
+
+
+# TODO:
+if __name__ == "__main__":
+    # simple example
+    pass
