65c00e53028611be06ba82abc0b64a09502c2ca6
Jigang Kim <jgkim2020@snu.ac.kr>
2020-07-20 21:31:23 +0900

    Add dependency for urx driver

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extras = {
     'mujoco': ['mujoco_py>=1.50, <2.0', 'imageio'],
     'robotics': ['mujoco_py>=1.50, <2.0', 'imageio'],
     'custom': ['mujoco_py>=1.50, <2.0', 'imageio'],
-    'real': [],
+    'real': ['math3d'],
 }
 
 # Meta dependency groups.
