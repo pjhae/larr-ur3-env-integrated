4f2d32f73b281a9a62f1239b7e1df5d011aadb15
pjhae <bdfire1234@snu.ac.kr>
2023-08-03 19:01:52 +0900

    sim_env_dual_to_single

--- gym_custom/envs/robotics/__init__.py
+++
@@ -1,3 +0,0 @@
-from gym_custom.envs.robotics.fetch_env import FetchEnv
-from gym_custom.envs.robotics.fetch.pick_and_place import FetchPickAndPlaceEnv
-from gym_custom.envs.robotics.fetch.gripper import FetchGripperEnv
